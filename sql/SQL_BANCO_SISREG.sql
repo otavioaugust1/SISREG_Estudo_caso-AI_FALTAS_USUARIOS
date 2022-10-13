50dfc033fb4819a5b91850533a5c24162aeeaf83
Otavio Augusto <otavioaugust@gmail.com>
2022-10-13 17:33:20 -0300

    version1.2

--- sql/SQL_BANCO_SISREG.sql
+++ sql/SQL_BANCO_SISREG.sql
@@ -17,7 +17,7 @@ USU.co_municipio_ibge							AS "IBGE_USUARIO",		-- CONFORME CADWEB
 (CASE 
 	WHEN USU.tp_raca = '01' THEN 'Branca'
 	WHEN USU.tp_raca = '02' THEN 'Negra'
-	WHEN USU.tp_raca = '03' THEN 'Amarela'
+	WHEN USU.tp_raca = '03' THEN 'Amarela'	
 	WHEN USU.tp_raca = '04' THEN 'Parda'
 	WHEN USU.tp_raca = '05' THEN 'Indigena'
 	ELSE 'Indefinida' END) 						AS "RACA_COR_USUARIO", 	-- CONFORME IBGE 
@@ -32,7 +32,7 @@ USU.nu_cep										AS "CEP_USUARIO",		-- CONFORME CADWEB
  	ELSE 'ATENDIDO FORA DO ESTADO DO USUARIO' END) AS "DESLOCAMENTO",	-- RELAÇÃO ESTUDO ANTERIOR
 MAR.co_cnes_ups									AS "CNES_EXECUTANTE",	-- CONFORME SISREG
 UPS.nu_cep										AS "CEP_UNID_EXECUTANTE",
-CBO.co_cbo 										AS "ESPECIALIDADE",		-- CONFORME SISREG
+ESP.ds_cbo_ocupacao								AS "ESPECIALIDADE",		-- CONFORME SISREG
 
 (CASE 
  	WHEN SOL.nu_carater = '0' 
@@ -57,8 +57,13 @@ JOIN	dbregulacao.tb_usuario 			AS USU ON (SOL.nu_cns_paciente = USU.nu_cns_pacie
 JOIN 	dbregulacao.rl_procedimento_cbo	AS CBO ON (SOL.co_pa_interno = CBO.co_pa_interno)
 JOIN 	dbregulacao.tb_pdr 				AS PDR ON (USU.co_municipio_ibge = PDR.co_municipio_ibge)
 JOIN	dbregulacao.tb_ups				AS UPS ON (MAR.co_cnes_ups = UPS.co_cnes_ups)
+JOIN	dbgeral.tb_cbo_ocupacao			AS ESP ON (CBO.co_cbo = ESP.co_cbo_ocupacao)	
 
-WHERE 	mar.dt_marcacao BETWEEN '2020-05-01' AND '2020-05-01' 
+
+
+WHERE 	
+		mar.dt_marcacao BETWEEN '2020-04-01' AND '2022-03-31' 
+	--	mar.dt_marcacao BETWEEN '2020-05-01' AND '2020-05-01' -- DATA USADA PARA O TESTE
 AND 	MAR.st_falta >= '0' 				-- APENAS SOLICITAÇÕES CONFIRMADAS OU COM FALTAS
 AND		SOL.st_situacao in ('A')			-- APENAS SOLICITAÇÕES AGENDADAS
 AND 	LENGTH(USU.nu_cep) = 8  			-- TRAZER APENAS PACIENTES COM CEP 
